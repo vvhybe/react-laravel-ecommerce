9f5012a8bb31a9d4a3de2fd33316d463842cd9ef
whybe7 <whybe7@hotmail.com>
2023-05-08 14:41:09 +0100

    sending CSRF token as a pass key through the api

--- server/routes/api.php
+++ server/routes/api.php
@@ -20,6 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->get('/csrf-cookie', function (Request $request) {
+    return response()->json(['message' => 'CSRF cookie has been set']);
+});
 
 // Auth APIs
 Route::post('/login', [AuthController::class, 'login']);
